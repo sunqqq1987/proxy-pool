e51b63f5e6395105c73665b9b7e3f2d4f4977d68
sunqqq1987 <sunqqq1987@163.com>
2018-05-15 09:30:41 +0800

    修改数据库路径，ubuntu获取与windows获取不一样

--- tools/setting.py
+++ tools/setting.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 from fake_useragent import UserAgent
+import os
 # set headers,use random
 headers_base = {'User-Agent': UserAgent().random}
 proxy_web_list = {
@@ -26,4 +27,7 @@ proxy_web_loop_number = 2
 
 thread_number = 20
 
-db = 'proxylist.db'
+db = os.path.dirname(__file__)+os.sep+'proxylist.db'
+# 数据库文件使用上面方式获取路径,使用os.getcwd()在Ubuntu上面获取的为用户主目录，获取不到脚本目录，具体原因搞不清楚
+# print(db)
+
